1fb1643455abb3ec057e86eafe93857024d8056b
BikashElangbam <58908215+BikashElangbam@users.noreply.github.com>
2023-01-27 15:40:46 +0530

    made changes in App

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import React from 'react';
 import './App.css';
-import { Routes,Route } from 'react-router-dom';
-import Login from './E-Commerce/Login';
-import ProductLists from './E-Commerce/ProductLists';
-import Registration from './E-Commerce/Registration';
-import Layout from './E-Commerce/Layout';
-import Home from './E-Commerce/Home';
-import About from './E-Commerce/About';
-import Cart from './E-Commerce/Cart';
-import ProfileList from './My_Practices/ProfileList';
 import { Routes, Route } from "react-router-dom";
+import Login from './E-Commerce/Component/Login';
+import ProductLists from './E-Commerce/Component/ProductLists';
+import Registration from './E-Commerce/Component/Registration';
+import Layout from './E-Commerce/Component/Layout';
+import Home from './E-Commerce/Component/Home';
+import About from './E-Commerce/Component/About';
+import Cart from './E-Commerce/Component/Cart'
+
+// import ProfileList from './My_Practices/ProfileList';
+// import TodoAppHooks from './My_Practices/TodoAppHooks';
 function App() {
   return (
     <>
-    {/* <FormValidation/> */}
-       
-        <Routes>
-          <Route path="/" element={<Layout/>}>
-            <Route index element={<Home/>} />
-            <Route path="/login" element={<Login/>}/> 
-            <Route path="/register" element={<Registration/>}/>
-            <Route path="/productlist" element={<ProductLists/>}/>
-            <Route path="/about" element={<About/>}/> 
-            <Route path="/cart" element={<Cart/>}/> 
-          </Route>
-        </Routes>
-        
-      
       <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>} />
+          <Route path="/login" element={<Login/>}/> 
+          <Route path="/register" element={<Registration/>}/>
+          <Route path="/productlist" element={<ProductLists/>}/>
+          <Route path="/about" element={<About/>}/> 
+          <Route path="/cart" element={<Cart/>}/> 
+        </Route>
+      </Routes>
+            
+
     </>
 
   );
